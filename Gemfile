607eab9d47317b47db0abfd8c29f6c3d97a9634c
Arthur Fincham <arthurfincham@me.com>
2021-10-04 15:40:11 +0100

    add views folder, add cat file with styled border

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '3.0.2'
+ruby '2.7.3'
 
 gem 'sinatra'
-gem 'webrick'
+gem 'sinatra-reloader', '~> 1.0'
--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
+require "bundler/setup"
 require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Secret message: x79asdfh11"
+end
+
+get '/cats' do
+  File.read('views/cats.erb')
 end
