5153f65c8d0d5078d65afda65c675620ba7ee4b8
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-05 09:14:30 +0100

    forms!

--- app.rb
+++ app.rb
@@ -10,6 +10,15 @@ get '/secret' do
   "Secret message: x79asdfh11"
 end
 
-get '/cats' do
+get '/random-cats' do
+  @name = ["Amigo", "Misty", "Almond"][rand(3)] #could go %w(Amigo Misty Almond)
+  erb(:index)
+end
+
+get '/named-cats' do
+  puts params
+  @name = params[:name] 
+  @age = params[:age]
+   #could go %w(Amigo Misty Almond)
   erb(:index)
 end
